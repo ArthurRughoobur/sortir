00d8e2cac672f56a4ec2b5c90c6cd25c35171377
aleclech2025 <alc.theclech@gmail.com>
2026-03-24 11:02:16 +0100

    Tymy Commit 24/03/26 --- fixtrure des users Admin, Adrien, Arthur

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,24 +2,61 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Campus;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function __construct(private UserPasswordHasherInterface $passwordHasher){
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
 
+    }
+
+    public function addCampus(ObjectManager $manager)
+    {
+        $campusList = ['Nantes', 'Niort', 'Quimper', 'Rennes'];
+        foreach ($campusList as $campus) {
+            $campusNew = new Campus();
+            $campusNew->setName($campus);
+            $manager->persist($campusNew);
+        }
+        $manager->flush();
     }
 
     public function load(ObjectManager $manager): void
     {
-
-//        $manager->flush();
+        $this->addCampus($manager);
+        $this->addUsers($manager);
     }
 
-    public function addUsers(ObjectManager $manager){
+    public function addUsers(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+        $campus = $manager->getRepository(Campus::class)->findAll();
+        $usernames = ['Admin', 'Arthur', 'Adrien'];
 
+        foreach ($usernames as $username) {
+            $user = new User();
+            $user->setUsername($username);
+            $user->setRoles(['ROLE_USER']);
+            $user->setPassword($this->passwordHasher->hashPassword($user, 123456));
+            $user->setName($faker->userName());
+            $user->setLastname($faker->lastName());
+            $user->setPhone($faker->phoneNumber());
+            $user->setEmail($faker->email);
+            $user->setActive($faker->boolean());
+            $user->setStudent($faker->boolean());
+            $user->setPhoto($faker->imageUrl());
+            $user->setCampus($faker->randomElement($campus));
+            $manager->persist($user);
 
+        }
+        $manager->flush();
     }
+
+
 }

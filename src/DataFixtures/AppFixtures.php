0f8fd7f9730547bb960330d1dc22b56a2f7402f5
aleclech2025 <alc.theclech@gmail.com>
2026-03-24 09:47:27 +0100

    Tymy Commit 24/03/26 --- Installation de composer fakerphp

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,22 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $passwordHasher){
+
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
-        $manager->flush();
+//        $manager->flush();
+    }
+
+    public function addUsers(ObjectManager $manager){
+
+
     }
 }

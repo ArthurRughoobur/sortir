fcfa33a0f13048e30038d351d2e06966769b1010
aleclech2025 <alc.theclech@gmail.com>
2026-03-24 13:53:19 +0100

    Tymy Commit 24/03/26 --- en dev page des event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 final class EventController extends AbstractController
 {
     #[Route('/', name: 'main_event')]
-    public function mainEvent(): Response
+    public function mainEvent(EventRepository $eventRepository): Response
     {
-        return $this->render('event/index.html.twig');
+        $events = $eventRepository->findAll();
+        return $this->render('event/index.html.twig',[
+            'events' => $events,
+        ]);
     }
 }

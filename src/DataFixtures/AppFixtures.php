bc194cc8f299e1049312a561eee79de8867ecd18
aleclech2025 <alc.theclech@gmail.com>
2026-03-24 11:40:25 +0100

    Tymy Commit 24/03/26 --- fixtrure des City

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Campus;
+use App\Entity\City;
 use App\Entity\Status;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -21,14 +22,15 @@ class AppFixtures extends Fixture
     {
         $campusList = ['Nantes', 'Niort', 'Quimper', 'Rennes'];
         foreach ($campusList as $campus) {
-            $campusNew = new Campus();
-            $campusNew->setName($campus);
-            $manager->persist($campusNew);
+            $newCampus = new Campus();
+            $newCampus->setName($campus);
+            $manager->persist($newCampus);
         }
         $manager->flush();
     }
 
-    public function addStatus(ObjectManager $manager){
+    public function addStatus(ObjectManager $manager)
+    {
         $statusList = ['En création', 'Ouverte', 'Clôturée', 'En cours', 'Terminée', 'Annulée', 'Historisée'];
         foreach ($statusList as $status) {
             $newStatus = new Status();
@@ -38,11 +40,32 @@ class AppFixtures extends Fixture
         $manager->flush();
     }
 
+    public function addCity(ObjectManager $manager)
+    {
+        //Niort = 79000, Rennes = 35000, Nantes = 44000, Quimper = 29000
+        $cities = [
+            'Nantes' => '44000',
+            'Niort' => '79000',
+            'Quimper' => '29000',
+            'Rennes' => '35000',
+        ];
+
+        foreach ($cities as $name => $zipcode) {
+            $newCity = new City();
+            $newCity->setName($name);
+            $newCity->setZipcode($zipcode);
+            $manager->persist($newCity);
+        }
+
+        $manager->flush();
+    }
+
     public function load(ObjectManager $manager): void
     {
         $this->addCampus($manager);
         $this->addUsers($manager);
         $this->addStatus($manager);
+        $this->addCity($manager);
     }
 
     public function addUsers(ObjectManager $manager)
